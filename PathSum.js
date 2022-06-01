59a41c6a0e91c2d068b0afb89478ce7c5d6c5dc5
bird.ron <bird.ron@pm.me>
2022-06-01 15:16:39 -0700

    Revised Path Sum
    Removed useless comments.
    
    Changed the formatting so that comments are on their own line.

--- PathSum.js
+++ PathSum.js
@@ -1,11 +1,10 @@
-/**
- * @param {TreeNode} root
- * @param {number} targetSum
- * @return {boolean}
- */
 const hasPathSum = (root, target) => {
-  if (root === null) return false; // return false if the tree is empty
-  target -= root.val; // update target based on the value of the current node
-  if (root.left === null && root.right === null) return target === 0; // if we're at a leaf node, return true if the target has been reached, false if it hasn't
-  return hasPathSum(root.left, target) || hasPathSum(root.right, target); // we're not at a leaf node; check the rest of the tree
+  // return false if the tree is empty
+  if (root === null) return false;
+  // update target based on the value of the current node
+  target -= root.val;
+  // if we're at a leaf node, return true if the target has been reached, false if it hasn't
+  if (root.left === null && root.right === null) return target === 0;
+  // we're not at a leaf node; check the rest of the tree
+  return hasPathSum(root.left, target) || hasPathSum(root.right, target);
 };
